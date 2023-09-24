f8c794fc380b79d9df7ec701f94428c11c788487
kanev-seth <kanevseth@outlook.com>
2023-09-25 03:29:55 +0530

    Update index.js
    oh i had no idea github pages cannot host node.js web applications

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.get("/", function(req, res)
 {
     // console.log(req);
     res.sendFile(__dirname + "/index.html");
-    // res.send(__dirname);
     //console.log(__dirname); //getting the exact location of the directory where this backend code is located(in which port, which server, etc).
 });
 
@@ -20,8 +19,8 @@ app.post("/", function(req, res)
     var num1 = Number(req.body.n1); //from the body of the request that has been created by the urlencoded method of body-parser, we need to get the access to the n1 data.
     var num2 = Number(req.body.n2);
     var result = num1 + num2;
-    res.send("hi");
-});
+    res.send("Addition of the two numbers : " + result);
+})
 
 app.get("/about", function(req, res)
 {
